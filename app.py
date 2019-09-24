fe396384fe09fd4e7c5c05b6634ab513220f0e28
Subas Subedi <ssubas999@gmail.com>
2019-09-24 01:15:58 +0000

    Messages displayd using lists rendering +Integration of postgresql using psycopg2 library

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ socketio = flask_socketio.SocketIO(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ssubas999:1Maryland1@localhost/postgres'
 db = flask_sqlalchemy.SQLAlchemy(app)
 
+counter = 0
+
 @app.route('/')
 def hello():
     return flask.render_template('index.html')
@@ -16,10 +18,11 @@ def hello():
 @socketio.on('connect')
 def on_connect():
     print('Someone connected!')
-    
+
 @socketio.on('disconnect')
 def on_disconnect():
     print('Someone disconnected!')
+
     
 # *** Server received new message event sent by client ***
 @socketio.on('new message')
@@ -28,15 +31,9 @@ def on_new_message(data):
     server_received_name = data['user_name']
     server_received_message = data['user_message']
     print(server_received_name, server_received_message)
+    
     # ***************************
-    # message = models.Message(server_received_name, server_received_message)
-    # models.db.session.add(message)
-    # models.db.session.commit()
-    
-    # messages = models.Message.query.all()
-    # print(messages)
-    
-    # Connect to the database
+    # Connect to the postgresql using psycopg2
     con = psycopg2.connect(database="postgres", user="ssubas999", password="1Maryland1", host="127.0.0.1", port="5432")
     print("Database opened successfully")
     
@@ -47,31 +44,27 @@ def on_new_message(data):
     print("Record inserted successfully")
     con.close()
     
-    # Connect to the database
+    # Connect to the database again
     con = psycopg2.connect(database="postgres", user="ssubas999", password="1Maryland1", host="127.0.0.1", port="5432")
     print("Database opened successfully")
     
-    #Retrieving the data from database
+    # Retrieving the data from database
     cur = con.cursor()
     cur.execute("SELECT user_name, user_message from Message")
     rows = cur.fetchall()
     
+    new_list = list(rows)
+    print(new_list)
     for name_message_list in rows:
         # print(name_message_list)
         name = name_message_list[0]
         message = name_message_list[1]
         # print(name)
         # print(message)
-        
-        
-    html = ['<li>' + name + '</li><li>' + message + '</li>' for m in name_message_list]
-    messages = '<ul>' + ''.join(html) + '</ul>'
-    print(messages)
-    # return messages
 
     # ***************************
     # *** username and message sent from server to every client ***
-    socketio.emit('message received', {'user_name': server_received_name, 'user_message': server_received_message});
+    socketio.emit('message received', {'user_name': server_received_name, 'user_message': server_received_message, 'messages_list': new_list});
 
 
 
