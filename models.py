e75a374b2f9ae3161d06724db734786bc8f5204e
Subas Subedi <ssubas999@gmail.com>
2019-09-27 14:44:52 +0000

    Added .gitignore and minor fixed

--- models.py
+++ models.py
@@ -3,15 +3,15 @@ import os, flask_sqlalchemy, app
 
 
 # app.app = app modules app variable
-app.app.config['SQLALCHEMY_DATABASE_URI']  = os.getenv('DATABASE_URL')
-# app.app.config['SQLALCHEMY_DATABASE_URI']  = 'postgresql://ssubas999:1Maryland1@localhost/postgres'
+# app.app.config['SQLALCHEMY_DATABASE_URI']  = os.getenv('DATABASE_URL')
+app.app.config['SQLALCHEMY_DATABASE_URI']  = 'postgresql://ssubas999:1Maryland1@localhost/postgres'
 db = flask_sqlalchemy.SQLAlchemy(app.app)
 
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     user_name = db.Column(db.String(25))
-    user_message = db.Column(db.String(200))
+    user_message = db.Column(db.String(500))
     
     def __init__(self, name, message):
         self.user_name = name
